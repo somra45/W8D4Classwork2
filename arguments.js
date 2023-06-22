39ac59a741662716fb621241c481887522aa4438
root <somra45@gmail.com>
2023-06-22 13:35:00 -0400

    finish my bind

--- arguments.js
+++ arguments.js
@@ -19,22 +19,65 @@ const sum2 = function(...args) {
 
 // sum2(1,2,3,4);
 
-Array.prototype.myBind = function () {
+Function.prototype.myBind = function () {
     let that = this;
-    let args = Array.prototype.slice.call(arguments).slice(1);
+    let args = Array.prototype.slice.call(arguments).slice(1)
     let ctx = arguments[0];
 
     return function () {
-        if (!(arguments === undefined)) {
+        if (typeof arguments != `undefined`) {
             let callArgs = Array.prototype.slice.call(arguments);
-            callArgs.concat(args);
-            return that.apply(ctx,callArgs);
+            let finalArgs = args.concat(callArgs);
+            return that.apply(ctx,finalArgs);
         } else {
             return that.apply(ctx,args);
         }
     };
 };
 
+Function.prototype.myBind1 = function (...bindArgs) {
+    let that = this;
+    let ctx = bindArgs[0];
+    let args = bindArgs.slice(1);
+    return function (...callArgs) {
+        return that.apply(ctx, args.concat(callArgs));
+    };
+};
 
 
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    says(sound, person) {
+      console.log(`${this.name} says ${sound} to ${person}!`);
+      return true;
+    }
+  }
+  
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const markov = new Cat("Markov");
+  const pavlov = new Dog("Pavlov");
+  
 
+//   markov.says("meow", "Ned");
+
+// markov.says.myBind(pavlov, "meow", "Kush")();
+// markov.says.myBind1(pavlov, "meow", "Kush")();
+
+// markov.says.myBind(pavlov)("meow", "a tree");
+// markov.says.myBind1(pavlov)("meow", "a tree");
+
+// markov.says.myBind(pavlov, "meow")("Markov");
+// markov.says.myBind1(pavlov, "meow")("Markov");
+
+// const notMarkovSays = markov.says.myBind(pavlov);
+// notMarkovSays("meow", "me");
+// const notMarkovSays = markov.says.myBind1(pavlov);
+// notMarkovSays("meow", "me");
