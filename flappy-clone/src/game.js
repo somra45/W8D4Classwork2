91b5c6f956a97ee9a91acb4316ffd562df5c42da
root <somra45@gmail.com>
2023-06-22 15:11:40 -0400

    start flappybird

--- flappy-clone/src/game.js
+++ flappy-clone/src/game.js
@@ -1,6 +1,17 @@
+import Level from `./level`
+
 export default class FlappyBird {
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+  }
+
+  animate(ctx) {
+    Level.animate(ctx)
+  }
+
+  restart() {
+    this.level = new Level(this.dimensions) 
+    this.animate(ctx)
   }
 }
